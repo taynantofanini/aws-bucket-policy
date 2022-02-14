b72fc23f28000512229f319706461a0aa05f93e4
Taynan Tofanini <o2b.taynan@webmotors.com.br>
2022-02-14 17:11:56 -0300

    habilita, atualiza ou passa

--- s3_bucket_functions.py
+++ s3_bucket_functions.py
@@ -28,30 +28,35 @@ def put_buckets3_policy(listed_buckets):
         policy = '{"Version": "2012-10-17","Statement": [{"Sid": "Statement1","Effect": "Deny","Principal": "*","Action": "s3:*","Resource": ["arn:aws:s3:::' + name + '",' + '"arn:aws:s3:::' + name + '/*"],"Condition": {"Bool": {"aws:SecureTransport": "false"} } }]}'
         response = clients3.put_bucket_policy(Bucket=name, Policy=policy)
 
-def buckets3_logging(listed_buckets):
+def get_buckets3_logging(listed_buckets):
+    bucket_log_name = "lab-taynan-logs"
     for bucket in listed_buckets:
         bucket_logging = clients3.get_bucket_logging(Bucket=bucket)
         if "LoggingEnabled" not in bucket_logging.keys():
-            bucket_logging_put = clients3.put_bucket_logging(
+            put_buckets3_logging(bucket, bucket_log_name)
+            print("Logging habilitado para o bucket '{}' ".format(bucket))
+        else:
+            if bucket_logging["LoggingEnabled"]["TargetBucket"] != bucket_log_name:
+                put_buckets3_logging(bucket, bucket_log_name)
+                print("Logging atualizado para o bucket '{}' ".format(bucket))
+    print("Todos os buckets foram atualizados e estão com o 'Server Access Logging' ativos e atualizados")
+
+def put_buckets3_logging(bucket, bucket_log_name):
+    bucket_logging_put = clients3.put_bucket_logging(
                 Bucket=bucket, 
                 BucketLoggingStatus={
                     "LoggingEnabled": {
-                        "TargetBucket": bucket,
-                        "TargetPrefix": "logs"
+                        "TargetBucket": bucket_log_name,
+                        "TargetPrefix": bucket
                     }
                 })
-            print("Logging habilitado com sucesso para o bucket '{}' ".format(bucket))
-        else:
-            print("Logging previamente habilitado para o bucket '{}' ".format(bucket))
-    print("Todos os buckets foram atualizados e estão com o 'Server Access Logging' ativos")
 
 
 if __name__ == "__main__":
-    
     #listed_buckets = list_buckets_names()
     listed_buckets = ["taynan-lab"] ## list definition for manual test
     try:
         get_bucket_policy(listed_buckets)
     except:
         put_buckets3_policy(listed_buckets)
-    buckets3_logging(listed_buckets)
+    get_buckets3_logging(listed_buckets)
